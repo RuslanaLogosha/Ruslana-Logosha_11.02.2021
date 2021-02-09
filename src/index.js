76ba0379fa3f02e65d8971a4aaa9de973036b023
Ruslana Logosha <ruslanalogosha@gmail.com>
2021-02-09 18:40:47 +0200

    fixes checkbox storage

--- src/index.js
+++ src/index.js
@@ -3,66 +3,50 @@ import { fetchMovies, fetchMoviesById } from './js/apiService';
 import moviesTpl from './templates/moviesTemplate.hbs';
 import favouriteTpl from './templates/listItemTemplate.hbs';
 
-// import { parseWithoutProcessing } from 'handlebars';
-
 const refs = getRefs();
 
 function initStorageDelayed() {
   setTimeout(() => {
     var favorites = JSON.parse(localStorage.getItem('favorites')) || [];
-    // add class 'fav' to each favorite
-    favorites.forEach(function (favorite) {
-      document.getElementById(favorite).className = 'checked';
+
+    console.log(favorites);
+    favorites.forEach(id => {
+      fetchFavouriteMoviesList(id);
+      const liElem = document.getElementById(id);
+      const divElem = liElem.firstElementChild;
+      const imgElem = divElem.firstElementChild;
+      const inputElem = imgElem.nextElementSibling;
+      inputElem.classList.add('checked');
     });
-    // register click event listener
+
     document
       .querySelector('.gallery-list')
-      .addEventListener('change', function (e) {
-        var id = e.target.id,
+      .addEventListener('click', function (e) {
+        const id = e.target.id,
           item = e.target,
           index = favorites.indexOf(id);
-        // return if target doesn't have an id (shouldn't happen)
+        console.log(item);
+
         if (!id) return;
+
         // item is not favorite
         if (index == -1) {
           favorites.push(id);
-          item.className = 'checked';
+          item.classList.add('checked');
+          console.log(favorites);
+
           fetchFavouriteMoviesList(id);
 
           // item is already favorite
         } else {
           favorites.splice(index, 1);
-          item.className = '';
+          item.classList.remove('checked');
         }
         // store array in local storage
         localStorage.setItem('favorites', JSON.stringify(favorites));
       });
-    // const favourites = JSON.parse(localStorage.getItem('favourites')) || [];
-
-    // document
-    //   .querySelector('.gallery-list')
-    //   .addEventListener('change', function (e) {
-    //     const id = e.target.id,
-    //       item = e.target,
-    //       index = favourites.indexOf(id);
-    //     const checked = item.checked;
-    //     if (checked) {
-    //       favourites.push(id);
-    //       fetchFavouriteMoviesList(id);
-    //     }
-    //     if (!checked) {
-    //       favourites.splice(index, 1);
-    //     }
-
-    //     localStorage.setItem('favourites', JSON.stringify(favourites));
-    //     console.log(favourites);
-    //   });
   }, 1000);
 }
-
-// const favouritesList = JSON.parse(localStorage.getItem('favourites'));
-// if (favouritesList) {
-// }
 
 initStorageDelayed();
 
